fef08b9184f941fe6e3645ae19dea4b8b667bbac
Erik Gustavo Comelli <erikcomelli@hotmail.com>
2019-02-12 23:35:42 -0200

    configuração datatables

--- ProjetoLoja/Controllers/CategoryController.cs
+++ ProjetoLoja/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using ProjetoLoja.Domain;
 using ProjetoLoja.Domain.DTO;
 using ProjetoLoja.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -18,19 +19,75 @@ namespace ProjetoLoja.Web.Controllers
 
         public IActionResult Index()
         {
-            var categories =_categoryService.GetAll().ToList();
-            var categoriesDto = new List<CategoryDTO>();
-            foreach (var item in categories)
-            {
-                categoriesDto.Add(SetCategoryDTO(item));
-            }
-            return View(categoriesDto);
+            //var categories =_categoryService.GetAll().ToList();
+            //var categoriesDto = new List<CategoryDTO>();
+            //foreach (var item in categories)
+            //{
+            //    categoriesDto.Add(SetCategoryDTO(item));
+            //}
+            //return View(categoriesDto);
+            return View();
         }
 
         public IActionResult CreateOrEdit()
         {
             
             return View();
+        }
+
+        public IActionResult GetAllCategories()
+        {
+            try
+            {
+                var draw = HttpContext.Request.Form["draw"].FirstOrDefault();
+
+                // Skip number of Rows count  
+                var start = Request.Form["start"].FirstOrDefault();
+
+                // Paging Length 10,20  
+                var length = Request.Form["length"].FirstOrDefault();
+
+                // Sort Column Name  
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+
+                // Sort Column Direction (asc, desc)  
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+
+                // Search Value from (Search box)  
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+                //Paging Size (10, 20, 50,100)  
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+
+                int recordsTotal = 0;
+
+                // getting all Customer data  
+                var customerData = _categoryService.GetAll();
+                //Sorting  
+                //if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                //{
+                //    customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection);
+                //}
+                //Search  
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    customerData = customerData.Where(m => m.Name == searchValue);
+                }
+
+                //total number of rows counts   
+                recordsTotal = customerData.Count();
+                //Paging   
+                var data = customerData.Skip(skip).Take(pageSize).ToList();
+                //Returning Json Data  
+                return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data });
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         [HttpPost]
---
+++ ProjetoLoja/wwwroot/js/category/index.js
@@ -0,0 +1,45 @@
+﻿$(function () {
+    $("#categoriesDT").dataTable({
+        "oLanguage": {"sUrl": "../../lib/datatables/portuguese.txt"},
+        "processing": true,
+        "serverSide": true,
+        "filter": true,
+        "orderMulti": false,
+        "ajax": {
+            "url": "Category/GetAllCategories",
+            "type": "POST",
+            "dataType":"json"
+        },
+        //"columnsDefs": [{
+        //    "targets": [0],
+        //    "visible": false,
+        //    "searcheble":false
+        //}],
+        "columns": [
+            { "data": "id", "name": "Id", "autoWidth": true },
+            { "data": "name", "name": "Nome", "autoWidth": true },
+            {
+                "render": function (data, type, full, meta)
+                {
+                    return '<a class="btn btn-info" href="/Category/CreateOrEdit/' + full.Id + '">Editar</a>';
+                }
+            },
+            {
+                data: null, render: function (data, type, row)
+                {
+                    return "<a href='#' class='btn btn-danger' onclick=ConfirmDelete('" + row.Id + "'); >Excluir</a>";
+                }
+            }                        
+        ]
+    });
+
+    function ConfirmDelete(id) {
+        //confirm if user wants to exclude data
+        Delete(id);
+    }
+
+    function Delete(id) {
+        //call js to delete
+    }
+
+});
